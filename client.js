275ba2d3045d575055c20519371ea53f967dc279
Daniel Legan <legan.daniel@gmail.com>
2022-09-20 13:44:31 -0500

    modified calculateIndividualEmployeeBonus function

--- client.js
+++ client.js
@@ -34,20 +34,6 @@ const employees = [
 
 console.log('array of employee data: ',  employees );
 
-function newEmployeeObject(employee){
-  //delete employee.employeeNumber;
-  //delete employee.annualSalary;
-  //delete employee.reviewRating;
-  employee.bonusPercentage = 2;
-  employee.totalCompensation = 2;
-  employee.totalBonus = 4;
-
-  return employee;
-}
-
-let result = newEmployeeObject(employees[0]);
-console.log(result);
-
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
@@ -63,9 +49,16 @@ console.log(result);
 // This function will calculate 1 employee's bonus!
 //
 function calculateIndividualEmployeeBonus( employee ) {  
+  
+
   // your logic here
 if(employee[0].reviewRating === 3){
-  employee[0].annualSalary = employee[0].annualSalary*1.04;
+  employee[0].bonusPercentage = 0.04;
+  employee[0].totalCompensation = employee[0].annualSalary * employee[0].bonusPercentage;
+  employee[0].totalBonus = (1*employee[0].annualSalary) + (1*employee[0].totalCompensation);
+  //console.log('Total compensation is:', employee[0].totalCompensation);
+  //console.log('Total bonus percentage', employee[0].bonusPercentage);
+  //console.log('Total bonus:', employee[0].totalBonus);
 }
 
 if(employee[0].reviewRating === 4){
@@ -79,6 +72,11 @@ if(employee[0].reviewRating === 5){
 if(employee[0].employeeNumber.length === 4){
   console.log('the employee numbner is 4 digits long');
   employee[0].annualSalary = employee[0].annualSalary*1.05;
+}
+
+if(employee[2].annualSalary > 65000){
+  employee[2].annualSalary*0.99;
+  console.log('the employee salary is greater than 65000')
 }
 
   return employee[0];
